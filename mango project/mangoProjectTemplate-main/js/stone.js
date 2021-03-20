071b16afe3472c2705a4495e95429f64828e1cdc
shivam12345-spec <77944866+shivam12345-spec@users.noreply.github.com>
2021-03-20 19:25:43 -0400

    Add files via upload

---
+++ mango project/mangoProjectTemplate-main/js/stone.js
@@ -0,0 +1,31 @@
+class stone{
+	stoneconstructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution :0,
+            friction :1,
+            density: 1.2
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage('images/stone.png')
+		this.body=Bodies.circle(this.x, this.y, this.r, options)
+		World.add(world, this.body);
+	}
+
+	display()
+	{
+		var stoneObj=this.body.position;	
+		push()
+		translate(stoneObj.x, stoneObj.y);
+		// rectMode(CENTER);
+		rotate(this.body.angle)
+		fill(51,51,51)
+		imageMode(CENTER);
+		ellipseMode(CENTER);
+		//image(this.image, 0,0,this.r*2, this.r*2)
+		pop()
+ }
+}
